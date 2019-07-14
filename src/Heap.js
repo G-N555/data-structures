8c1340abdd97d45c7202f4a8d421b4fb743f177f
Go Nakano <g.nakano555@gmail.com>
2019-07-14 12:58:25 +0900

    implemented insert on Heap

--- src/Heap.js
+++ src/Heap.js
@@ -1,1 +1,26 @@
-class Heap {}
+class Heap {
+  constructor() {
+    this.storage = [];
+  }
+
+  insert(value) {
+    this.storage.push(value);
+    const bubbleUp = () => {
+      let index = this.storage.length - 1;
+      while (index > 0) {
+        let target = this.storage[index];
+        let parentInd = Math.floor((index - 1) / 2);
+        let parent = this.storage[parentInd];
+        if (parent > target) break;
+        this.storage[index] = parent;
+        this.storage[parentInd] = target;
+        index = parentInd;
+      }
+    };
+    bubbleUp();
+  }
+
+  removeMax() {}
+}
+
+module.exports = Heap;
